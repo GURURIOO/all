41d22e63fb2f60218cd149ddf3b0eca7d3f950b0
GURURIOO <94834670+GURURIOO@users.noreply.github.com>
2022-07-22 12:35:29 +0530

    Add files via upload

---
+++ numinstring.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class numinstring {
+
+	public static Scanner sc= new Scanner(System.in);
+	public static void main(String[] args) {
+		String a=sc.nextLine();
+		char [] n=a.toCharArray();
+		for(int i=0;i<a.length();i++) {
+			if(n[i]>47&&n[i]<58) {
+				System.out.print(n[i]+"  ");
+			}
+		}
+
+	}
+}
